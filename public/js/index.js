3b3202f2d6efe6bbc997dc0009ce4372b3dc46bb
Wesley <wesleyrasada@gmail.com>
2017-04-19 23:28:04 -0700

    Add form input

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,21 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
   console.log('New message! ', message);
 });
+
+socket.emit('createMessage', {
+  from: 'Frank',
+  text: 'Hello!'
+}, function (data) {
+  console.log('Got it!', data);
+});
+
+$('#messageForm').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function () {
+
+  });
+});
